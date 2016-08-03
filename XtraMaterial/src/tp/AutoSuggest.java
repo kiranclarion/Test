65b2c34682edeab0409336d46b4e3ce6852df5a3
Kiran Hole <Kiran Hole@192.168.252.9>
2016-08-03 17:30:17 +0530

    updated

--- XtraMaterial/src/tp/AutoSuggest.java
+++ XtraMaterial/src/tp/AutoSuggest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 public class AutoSuggest {
-	
+	// test auto
 		WebDriver driver;
 		WebDriverWait wait;
 
